52505fa778f4b101153fe1490bd69419dde3665d
Nick Konev <velosipedist.org@gmail.com>
2016-10-28 15:46:42 +0300

    Add examples

---
+++ examples/index.php
@@ -0,0 +1,14 @@
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php") ?>
+<?php
+
+Beatrix\get('/', function () {
+
+});
+
+Beatrix\route('/', function () {
+
+});
+
+?>
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php") ?>
