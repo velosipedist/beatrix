b34425b9733c07ff6cd690d687c267222a5e3751
Nick Konev <velosipedist.org@gmail.com>
2016-10-28 15:31:35 +0300

    Testing improvements

---
+++ src/bitrix_stubs.php
@@ -0,0 +1,14 @@
+<?php
+// !!! Do not include me into bulid
+if (defined('BX_ROOT')) {
+    return;
+}
+define('SITE_TEMPLATE_ID', 'main');
+function GetMessage($name, $aReplace)
+{
+    return '';
+}
+
+function AddEventHandler($module, $ev, $handler, $position = 0)
+{
+}
